b7442d030d6f6577705fe294ac9caeadfaefed74
hujunlei <hujunlei@yunzhangfang.com>
2022-01-25 17:35:13 +0800

    网站下拉框和关键词下拉框逻辑优化，添加关键词消除功能

--- src/component/Search/index.tsx
+++ src/component/Search/index.tsx
@@ -8,11 +8,22 @@ import { SearchUrlList } from './SearchUrlList'
 
 const Search = () => {
   const [searchUrl, setSearchUrl] = useLocalStorageState('currentSearch', defaultCurrentSearch)
-  const [keyword, setKeyword] = useState('') 
+  const [keyword, setKeyword] = useState('')
   const [showSearchUrlList, setShowSearchUrlList] = useState(false)
-
+  const [keywordVisible, setKeywordVisible] = useState(false)
+  useEffect(() => {
+    setKeywordVisible(!!keyword)
+    return () => setKeywordVisible(false)
+  }, [keyword])
+  useEffect(() => {
+    const callback = () => setShowSearchUrlList(false)
+    document.addEventListener('click', callback, false)
+    return () => {
+      document.removeEventListener('click', callback, false)
+    }
+  }, [])
   // go to search page
-  const handleSearchGo = (e : any) => {
+  const handleSearchGo = (e: any) => {
     e.preventDefault()
     const actionUrl = searchUrl.url.replace('$keyword$', keyword)
     window.open(actionUrl)
@@ -20,35 +31,40 @@ const Search = () => {
 
   return (
     <div className="Search">
-      { searchUrl && 
+      {searchUrl &&
         <div className="SearchBar">
-          <div className="SearchUrl rowcenter">
-            <span className="rowcenter" onClick={ () => setShowSearchUrlList(!showSearchUrlList) }>
-              { searchUrl.title }
+          <div className="SearchUrl rowcenter urlTag" onClick={(e) => {
+            e.stopPropagation()
+            setShowSearchUrlList(!showSearchUrlList);
+          }}>
+            <span className="rowcenter">
+              {searchUrl.title}
               {
                 showSearchUrlList ?
-                <Right theme="outline" size="19" fill="#eee"/>
-                :
-                <Down theme="outline" size="19" fill="#eee"/>
+                  <Down theme="outline" size="19" fill="#eee" /> :
+                  <Right theme="outline" size="19" fill="#eee" />
               }
             </span>
-            { showSearchUrlList && <SearchUrlList setShow={ setShowSearchUrlList } setSearchUrl={ setSearchUrl } /> }
-            { keyword.length > 0 && <SearchSuggestion keyword={ keyword } setKeyword={ setKeyword }/> }
+            {showSearchUrlList && <SearchUrlList setShow={setShowSearchUrlList} setSearchUrl={setSearchUrl} />}
+            {keywordVisible && <SearchSuggestion keyword={keyword} setKeyword={setKeyword} />}
           </div>
-          <form className="center" onSubmit={ handleSearchGo }>
-            <input 
-              type="text" 
-              value={ keyword } 
-              placeholder={ searchUrl.describtion }
-              onChange={ e => setKeyword(e.target.value) }
+          <form className="center" onSubmit={handleSearchGo}>
+            <input
+              type="text"
+              value={keyword}
+              placeholder={searchUrl.describtion}
+              onChange={e => setKeyword(e.target.value)}
+              onFocus={() => setKeywordVisible(!!keyword)}
+              onBlur={() => setKeywordVisible(false)}
             ></input>
           </form>
-          <div className="SearchButton center" onClick={ handleSearchGo }>
-            <svg width="19" height="19" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="50" height="50" fill="white" fill-opacity="0.01"/><path d="M21 38C30.3888 38 38 30.3888 38 21C38 11.6112 30.3888 4 21 4C11.6112 4 4 11.6112 4 21C4 30.3888 11.6112 38 21 38Z" fill="none" stroke="#fff" stroke-width="3" stroke-linejoin="round"/><path d="M26.6568 14.3431C25.2091 12.8954 23.2091 12 21 12C18.7909 12 16.7909 12.8954 15.3431 14.3431" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/><path d="M33.2218 33.2218L41.7071 41.7071" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>
+          <div className="SearchButton center" >
+            <span onClick={() => setKeyword('')}> X</span>
+            <svg onClick={handleSearchGo} width="19" height="19" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="50" height="50" fill="white" fill-opacity="0.01" /><path d="M21 38C30.3888 38 38 30.3888 38 21C38 11.6112 30.3888 4 21 4C11.6112 4 4 11.6112 4 21C4 30.3888 11.6112 38 21 38Z" fill="none" stroke="#fff" stroke-width="3" stroke-linejoin="round" /><path d="M26.6568 14.3431C25.2091 12.8954 23.2091 12 21 12C18.7909 12 16.7909 12.8954 15.3431 14.3431" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" /><path d="M33.2218 33.2218L41.7071 41.7071" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" /></svg>
           </div>
         </div>
       }
-      </div> 
+    </div >
   )
 }
 
