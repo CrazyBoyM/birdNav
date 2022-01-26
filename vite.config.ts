ba73538f0f167f0937b66984cb734cc20a3b2f00
chenwencheng <chenwencheng@4399inc.com>
2022-01-26 11:20:00 +0800

    修改vite config - alias已经被淘汰，改成resolve.alias

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,9 @@ const htmlPlugin = () => {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), minifyHtml(), injectHtml()],
-  alias: {
-    '@': path.resolve(__dirname, './src'),
-  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
