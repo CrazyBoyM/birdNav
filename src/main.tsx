6c8c12b8be88adc5d73e4c3ce44b5646db9bb8d9
chenwencheng <chenwencheng@4399inc.com>
2022-01-26 11:34:46 +0800

    添加StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import '@/styles/reset.css'
-import '@/styles/var.css'
-import '@/styles/public.css'
-import '@/styles/index.css'
-import App from './App'
-import { localDataUpdate } from './utils/init'
+import React from "react";
+import ReactDOM from "react-dom";
+import "@/styles/reset.css";
+import "@/styles/var.css";
+import "@/styles/public.css";
+import "@/styles/index.css";
+import App from "./App";
+import { localDataUpdate } from "./utils/init";
 
-localDataUpdate()
+localDataUpdate();
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
